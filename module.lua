ddbb7831438d3e2c86e9a767a6daed2519ffc58f
revolume <70078003+LostInYourRange@users.noreply.github.com>
2022-01-01 18:52:41 +0100

    Create module.lua

---
+++ module.lua
@@ -0,0 +1,11 @@
+local services = {
+	rs = game:GetService("ReplicatedStorage");
+	ws = game:GetService("Workspace") or workspace;
+	ps = game:GetService("Players");
+	lg = game:GetService("Lighting");
+  lp = game:GetService("Players").LocalPlayer;
+	ts = game:GetService("TweenService");
+	runs = game:GetService("RunService");
+};
+
+return services;
