a414b2eea9859f17c15f18b8c32a88549d93f0c4
revolume <70078003+LostInYourRange@users.noreply.github.com>
2022-02-13 00:49:28 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -6,6 +6,12 @@ local services = {
         lp = game:GetService("Players").LocalPlayer;
 	ts = game:GetService("TweenService");
 	runs = game:GetService("RunService");
+	http = game:GetService("HttpService");
+	uis = game:GetService("UserInputService");
+	mouse = game:GetService("Players").LocalPlayer:GetMouse();
+	sss = game:GetService("ServerScriptStorage");
+	ss = game:GetService("ServerStorage");
+	playanim = function(anim) game:GetService("Players").LocalPlayer.Character.Humanoid:LoadAnimation(anim):Play();
 };
 
 return services;
